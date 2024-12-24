df6987882775ba53399648327c5ab91c1b2acc18
Alma Babic <alma.babic@tacta.io>
2024-12-24 14:58:21 +0100

    Section 6 - Lecture 144

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Ticket } from './ticket.model';
 
 @Component({
@@ -10,4 +10,19 @@ import { Ticket } from './ticket.model';
 })
 export class TicketComponent {
   data = input.required<Ticket>();
+
+  close = output();
+
+  detailsVisible = signal(false);   // detailsVisible is a reactive signal, initialized with false. This means that initially, the details are not visible.
+  onToggleDetails() {   // In the onToggleDetails method, you're toggling the value of detailsVisible
+    // First way - using set:
+    // this.detailsVisible.set(!this.detailsVisible());
+
+    // Second way - using update:
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
+
+  onMarkAsCompleted() {   // mark a ticket as completed (or closed)
+    this.close.emit();   // notify the parent component (or another component) that the ticket should be marked as completed, and some action might follow, such as updating the status of the ticket to "closed" in the parent component
+  }
 }
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -26,7 +26,25 @@ export class TicketsComponent {
 
     this.tickets.push(ticket);  // After the ticket object is created, it is added to the tickets array, which holds all the tickets. This array is then used to display the list of tickets in the parent component’s template.
   }
-
   // ticketData: an object that contains the title and text of the ticket. These values are passed from the child component (NewTicketComponent) when the form is submitted.
   // The ticketData is used to create a new Ticket object. The Ticket interface ensures that the created ticket has properties like title, request, id, and status.
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return  { ...ticket, status: 'closed' }
+      }
+      return ticket;
+    });
+  }
+  // onCloseTicket accepts an argument id, which is a string representing the unique identifier of the ticket that needs to be closed.
+  // this.tickets is an array of Ticket objects. The map() method is used to iterate over each ticket in the tickets array and create a new array with the modified tickets.
+  // map() takes a function that is called for each element in the array (ticket in this case).
+  // Inside the map() function, we check if the current ticket’s id matches the provided id.
+  // We then create a new ticket object using the spread operator ({ ...ticket, status: 'closed' }). This copies all properties from the original ticket but overrides the status property and sets it to "closed".
+  // This ensures that the ticket’s status is updated without modifying the original ticket object directly.
+  // If the id does not match, the ticket remains unchanged, and we simply return the ticket as-is.
+  // The result of map() is a new array where the ticket with the matching id has its status updated to "closed", and all other tickets remain the same.
+  // The tickets property of the component is then updated with this new array.
+  // { ...ticket, status: 'closed' }) creates a new object with the same properties as ticket, but with a modified status property.
 }
